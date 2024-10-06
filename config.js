c52aa5012d3b28e0e365e8a440f04103cf03d77a
Anish Raja <123381695+Anish888999@users.noreply.github.com>
2024-10-06 21:48:42 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+    titleWeb: "Heyy cutieeeeee",
+    introTitle: 'Heyy cutieeeeee',
+    introDesc: `I wanna ask you something`,
+    btnIntro: 'sure',
+    title: 'Did you fall in love at first sight? 🥰 after seeing me',
+    desc: 'Did you fall for that smile right away? which i have  ',
+    btnYes: 'I really like it ',
+    btnNo: 'No way ',
+    question: 'In a world of over 7 billion people, why do you love me?',
+    btnReply: 'Send to you <3',
+    reply: 'Love is love, whether you love or not <33333333',
+    mess: 'I knew it 🥰. Love you lots 😘😘',
+    messDesc: 'You are the only one for me',
+    btnAccept: 'Okayyyyy <3',
+    messLink: 'https://github.com' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+}
+
